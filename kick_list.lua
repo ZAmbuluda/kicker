350adafdc8fb9645a6e7a282728e18c752bf82df
ZAmbuluda <levraimlk78@gmail.com>
2026-02-25 19:49:56 +0100

    Update kick_list.lua

--- kick_list.lua
+++ kick_list.lua
@@ -1,5 +1,5 @@
 return {
-    ["Dersigmaboy12"] = "Your link is invalid!",
+    ["."] = "Your link is invalid!",
     ["."] = "Auth Dupe cant be bypassed. Add BobyDu009 and retry!"
     ["."] = "Please use a new link!"
 
