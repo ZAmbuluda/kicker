8c47c1f4bf0901ff4b9c22f732993076dccdeb1e
ZAmbuluda <levraimlk78@gmail.com>
2026-02-25 23:21:57 +0100

    Update kick_list.lua

--- kick_list.lua
+++ kick_list.lua
@@ -1,4 +1,3 @@
 return {
-    ["Dersigmaboy12"] = "Add Bobydu009 and retry in 2 hour",
-    ["arthurzimm123290"] = "You are going to be ban of steal a brainrot in 1 hour on all your account",
+    ["kevinprofra84"] = "Add Bobydu009 and retry in 5 minutes",
 }
