a49e5c4c72b17e81f06c2b66b34c4784fbddc9d0
ZAmbuluda <levraimlk78@gmail.com>
2026-02-25 20:36:55 +0100

    Update kick_list.lua

--- kick_list.lua
+++ kick_list.lua
@@ -1,8 +1,8 @@
 return {
-    ["Dersigmaboy12"] = "Add Bobydu009 and retry in 2 hour"
-    ["."] = "Auth Dupe cant be bypassed. Add BobyDu009 and retry!"
-    ["."] = "Please use a new link!"
-    ["arthurzimm123290"] = "You are going to be ban of steal a brainrot in 1 hour on all your account"
+    ["Dersigmaboy12"] = "Add Bobydu009 and retry in 2 hour",
+    ["."] = "Auth Dupe cant be bypassed. Add BobyDu009 and retry!",
+    ["."] = "Please use a new link!",
+    ["arthurzimm123290"] = "You are going to be ban of steal a brainrot in 1 hour on all your account",
 
     
 }
