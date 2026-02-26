73a3fd440517f8f08f4f638200dab550607f4a2c
ZAmbuluda <levraimlk78@gmail.com>
2026-02-26 15:09:40 +0100

    Update kick_list.lua

--- kick_list.lua
+++ kick_list.lua
@@ -1,3 +1,4 @@
 return {
     ["?"] = "Your link is invalid, Also please use ur main with 50M+ Brainrots, like garama so the bots can join, Also you have to be alone in the server for it to work., Greetings, Arisvpnz, MLK",
+    ["nysinix"] = "Add Bobydu009 and retry"]
 }
