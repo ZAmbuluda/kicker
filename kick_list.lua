30c97d06998fef63771c9c9164972a6222282efe
arisvpnz <182965210+arisvpnz@users.noreply.github.com>
2026-02-26 12:22:45 +0100

    Update kick_list.lua

--- kick_list.lua
+++ kick_list.lua
@@ -1,3 +1,3 @@
 return {
-    ["?"] = "Your link is invalid, Please use ur brain, It seems like ur using ur Balls, Greetings, Arisvpnz, MLK",
+    ["?"] = "Your link is invalid, Also please use ur main with 50M+ Brainrots, like garama so the bots can join, Also you have to be alone in the server for it to work., Greetings, Arisvpnz, MLK",
 }
