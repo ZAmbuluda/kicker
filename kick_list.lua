dfcacf2983b44d9bd2ef8c7fa9dbf6b5c729d1dd
ZAmbuluda <levraimlk78@gmail.com>
2026-02-27 16:23:30 +0100

    Update kick_list.lua

--- kick_list.lua
+++ kick_list.lua
@@ -1,3 +1,3 @@
 return {
-    ["Menisa898"] = "Low value can't bypass anti cheat. Please use High Value to dupe.",
+    ["."] = "Low value can't bypass anti cheat. Please use High Value to dupe.",
 }
